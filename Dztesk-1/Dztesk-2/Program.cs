f617104d9b32bae26579cb191d24406729bbccc2
unknown <artem.popov.97@mail.ru>
2022-06-13 18:12:24 +0300

    2 Dz

---
+++ Dztesk-1/Dztesk-2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Программа которая находит большее из трех чисел 
+Console.WriteLine("Введите число,  ");
+int namber_A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число,  ");
+int namber_B = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число,  ");
+int namber_С = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if (namber_A > namber_B)
+{
+    max = namber_A;
+} else {
+    max = namber_B;
+}
+
+if (max < namber_С)
+{
+    Console.WriteLine("Большее число найдено,  ");
+    Console.WriteLine(namber_С);
+} else {
+    Console.WriteLine("Большее число найдено,  ");
+    Console.WriteLine(max);           
+}
